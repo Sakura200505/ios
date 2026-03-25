a7e44f170ec443321f7f68552ba2fbdea733d8b6
Sakura200505 <yingshanben36@gmail.com>
2026-03-25 14:39:23 +0900

    ゲージ管理のスクリプトをmanagerに統一

--- Assets/Script/Manager/StatusManager.cs
+++ Assets/Script/Manager/StatusManager.cs
@@ -2,15 +2,46 @@ using UnityEngine;
 
 public class StatusManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static StatusManager Instance;
+
+    [Header("ステータス")]
+    public float maxHunger = 100f;
+    public float hunger = 100f;
+
+    private void Awake()
     {
-        
+        //Singleton化
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    //満腹度を減らす
+    public void DecreaseHunger(float amount)
     {
-        
+        hunger -= amount;
+        hunger = Mathf.Clamp(hunger, 0, maxHunger);
+
+        Debug.Log("満腹度：" + hunger);
+    }
+
+    //満腹度を回復
+    public void IncreaseHunger(float amount)
+    {
+        hunger += amount;
+        hunger = Mathf.Clamp(hunger, 0, maxHunger);
+
+        Debug.Log("満腹度：" + hunger);
+    }
+
+    //UI用（０～１に変化）
+    public float GetHungerNormalized()
+    {
+        return hunger / maxHunger;
     }
 }
--- Assets/Script/Player/foodGauge.cs
+++ Assets/Script/Player/foodGauge.cs
@@ -1,69 +1,13 @@
 using UnityEngine;
 using UnityEngine.UI;
+
 public class foodGauge : MonoBehaviour
 {
     [SerializeField] private Image foregroundGauge;
-    [SerializeField] private float maxValue = 1f;
-    private float current;
-    private void Start()
-    {
-        current = maxValue;
-    }
+
     private void Update()
     {
-        // :再生ボタン: 時間で減る（裏ゲージは減らない）
-        current -= 1f * Time.deltaTime;
-        current = Mathf.Clamp(current, 0f, maxValue);
-        foregroundGauge.fillAmount = current / maxValue;
-    }
-    // :再生ボタン: ボタンで回復
-    public void Heal(float amount)
-    {
-        current += amount;
-        current = Mathf.Clamp(current, 0f, maxValue);
+        //ステータスから値を取得して表示
+        foregroundGauge.fillAmount = StatusManager.Instance.GetHungerNormalized();
     }
 }
-
-//using System.Collections;
-//using UnityEngine;
-//public class GaugeController : MonoBehaviour
-//{
-//    [SerializeField] private GameObject _gauge;
-//    [SerializeField] private GameObject _graceGauge;
-//    [SerializeField] private int _HP = 100;
-//    private float _HP1;
-//    private float _waitingTime = 0.5f;
-//    // ▼自動減少のスピード（1秒に何HP減るか）
-//    [SerializeField] private float _decreasePerSecond = 1f;
-//    void Awake()
-//    {
-//        _HP1 = _gauge.GetComponent<RectTransform>().sizeDelta.x / _HP;
-//    }
-//    void Update()
-//    {
-//        // ▼毎秒HPを減らす
-//        AutoDecrease();
-//    }
-//    // ▼時間経過でゲージ減らす
-//    private void AutoDecrease()
-//    {
-//        float decreaseWidth = _HP1 * _decreasePerSecond * Time.deltaTime;
-//        RectTransform gauge = _gauge.GetComponent<RectTransform>();
-//        Vector2 now = gauge.sizeDelta;
-//        now.x -= decreaseWidth;
-//        now.x = Mathf.Max(now.x, 0);  // 0未満にならない
-//        gauge.sizeDelta = now;
-//        _graceGauge.GetComponent<RectTransform>().sizeDelta = now;
-//    }
-//    // ▼手動回復（ボタンから呼ぶ）
-//    public void Heal(int heal)
-//    {
-//        float healWidth = _HP1 * heal;
-//        RectTransform gaugeRect = _gauge.GetComponent<RectTransform>();
-//        Vector2 now = gaugeRect.sizeDelta;
-//        float maxWidth = _HP1 * _HP;
-//        now.x = Mathf.Min(now.x + healWidth, maxWidth);
-//        gaugeRect.sizeDelta = now;
-//        _graceGauge.GetComponent<RectTransform>().sizeDelta = now;
-//    }
-//}
