7f2f714142f26b315127d0aeca47ed719acaefdb
Sakura200505 <yingshanben36@gmail.com>
2026-03-26 14:42:05 +0900

    散歩機能のセーブ機能

--- Assets/Script/Manager/SaveManager.cs
+++ Assets/Script/Manager/SaveManager.cs
@@ -1,16 +1,48 @@
-using UnityEngine;
+//using UnityEngine;
+//using System;
+//using UnityEditor.Overlays;
+//using System.Data;
 
-public class SaveManager : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+//[SerializeField]
+//public class SaveDate
+//{
+//    public float hunger;
+//    public string lastSaveTime;
+//}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+//public class SaveManager : MonoBehaviour
+//{
+//    public static SaveManager instance;
+
+//    private string key = "SAVE_DATA";
+
+//    private void Awake()
+//    {
+//        if (instance == null) instance = this;
+//        else Destroy(gameObject);
+//    }
+
+//    public void Save(float hunger, bool isWalking, DateTime walkEndTime)
+//    {
+//        SaveData data =new SaveData();
+
+
+//        data.hunger = hunger;
+//        data.lastSaveTime = DataSetDateTime.Now.Tostring();
+
+//        data.isWalking = isWalking;
+//        data.walkEndTime = walkEndTime.ToString();
+
+//        string json = JsonUtility.ToJson(data);
+//        PlayerPrefs.SetString(key, json);
+//        PlayerPrefs.Save();
+//    }
+
+//    public SaveData Load()
+//    {
+//        if (!PlayerPrefs.HasKey(key)) return null;
+
+//        string json = PlayerPrefs.GetString(key);
+//        return JsonUtility.FromJson<SaveData>(json);
+//    }
+//}
--- Assets/Script/Walk/WalkManager.cs
+++ Assets/Script/Walk/WalkManager.cs
@@ -3,7 +3,7 @@
 
 //public class WalkManager : MonoBehaviour
 //{
-//   public static WalkManager Instance;
+//    public static WalkManager Instance;
 
 //    public bool isWalking = false;
 //    public DateTime endTime;
@@ -25,6 +25,94 @@
 
 //    private void Update()
 //    {
-//        if (isWalking && )
+//        if (isWalking && DateTime.Now >= endTime)
+//        {
+//            FinishWalk();
+//        }
+//    }
+
+//    //散歩開始（ボタンから呼ぶ）
+//    public void StartWalk()
+//    {
+//        if (!CanWalk()) return;
+
+//        isWalking = true;
+//        lastWalkTime = DateTime.Now;
+
+//        int duration = 10; //散歩の時間指定１０秒（何時間か掛けるようにするか検討）
+//        endTime = DateTime.Now.AddSeconds(duration);
+
+//        //散歩が終わった後の通知
+//        NotificationManager.Instance.ScheduleNotification
+//            ("散歩終了！", "ペットが帰ってきたよ！" duration);
+//    }
+
+//    //散歩が可能かの判定
+//    public bool CanWalk()
+//    {
+//        TimeSpan diff = DateTime.Now - lastWalkTime;
+//        return diff.TotalSeconds >= walkCooldownSeconds && !isWalking;
+//    }
+
+//    //散歩終了の判定
+//    void FinishWalk()
+//    {
+//        isWalking = false;
+
+//        Debug.Log("散歩終了");
+
+//        //アイテムを取得
+//        ItemManager.instance.GetRandamItem();
+//    }
+
+//    //残り時間のクールタイム
+//    public float GetRemainingCooldown()
+//    {
+//        float remain = walkCooldownSeconds - (float)(DateTime.Now - lastWalkTime).TotalSeconds;
+//        return Mathf.Max(0, remain);
+//    }
+
+//    //セーブ機能
+//    public void Save()
+//    {
+//        SaveManager.instance.Save
+//            (
+//                 StatusManager.Instance.hunger,
+//                 isWalking,
+//                 endTime
+//            );
+//    }
+
+//    //ロード機能
+//    void Load()
+//    {
+//        var data = SaveManager.Instance.Load();
+//        if (data == null) return;
+
+//        isWalking = data.IsWalking;
+
+//        if(isWalking)
+//        {
+//            endTime = DateTime.Parse(data.walkEndTime);
+
+//            //オフライン状態で終わっているかのチェック
+//            if (DateTime.Now >= endTime)
+//            {
+//                FinishWalk();
+//            }
+//            else
+//            {
+//                //残り時間で通知を再セット
+//                TimeSpan remaining = endTime - DateTime.Now;
+
+//                NotificationManager.Instance.ScheduleNotification
+//                    (
+//                      "散歩終了！",
+//                      "ペットが帰ってきたよ！",
+//                      (int)remaining.TotalSeconds
+//                    );
+//            }
+
+//        }
 //    }
 //}
