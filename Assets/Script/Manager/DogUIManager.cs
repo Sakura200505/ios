e01002fa56a7c3cce15bfa5e71a2d5ad36214397
Sakura200505 <yingshanben36@gmail.com>
2026-05-19 15:38:35 +0900

    ゲージの数値のよって表情が変化

---
+++ Assets/Script/Manager/DogUIManager.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DogUIManager : MonoBehaviour
+{
+    public Image dogImage;
+
+    [Header("Х\По")]
+    public Sprite normalSprite;
+    public Sprite hungrySprite;
+    public Sprite dirtySprite;
+    public Sprite stressSprite;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateDogFace();
+    }
+
+    void UpdateDogFace()
+    {
+        float hunger = StatusManager.Instance.hunger;
+        float clean = StatusManager.Instance.clean;
+        float stress = StatusManager.Instance.stress;
+
+        if (hunger <= 30)
+        {
+            dogImage.sprite = hungrySprite;
+        }
+
+        else if (clean <= 30)
+        {
+            dogImage.sprite = dirtySprite;
+        }
+
+        else if ( stress <= 70)
+        {
+            dogImage.sprite= stressSprite;  
+        }
+
+        else
+        {
+            dogImage.sprite = normalSprite;
+        }
+    }
+}
+
